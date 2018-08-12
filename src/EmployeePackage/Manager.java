dd5f0da6068b5618a454796ffb67c09a77d008c2
gun532 <gun532@outlook.com>
2018-08-12 13:14:08 +0300

    Check if it changed the commit

--- src/EmployeePackage/Manager.java
+++ src/EmployeePackage/Manager.java
@@ -3,36 +3,33 @@ package EmployeePackage;
 import java.sql.*;
 import java.util.Scanner;
 
-public class Manager extends Employee  {
+public class Manager extends Employee {
     public Manager(String name, int id, String phone, int accountNum, int branchNumber) {
         super(name, id, phone, accountNum);
         setBranchNumber(branchNumber);
         setJobPos(Profession.MANAGER);
     }
 
-    public void addEmploee()
-    {
+    public void addEmploee() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter Employee Type:");
         String emp = scan.next();
         Employee m1 = new Employee() {
         };
-        switch(emp)
-        {
+        switch (emp) {
             case "Manager":
-                m1 = new Manager("Guy",1243,"050-112211",234532,this.getBranchNumber());
+                m1 = new Manager("Guy", 1243, "050-112211", 234532, this.getBranchNumber());
                 break;
             case "Cashier":
-                m1 = new Cashier("Roy",342436,"050-253265",4646,this.getBranchNumber());
+                m1 = new Cashier("Roy", 342436, "050-253265", 4646, this.getBranchNumber());
                 break;
             case "Seller":
                 //m1 = new Seller("Ben",32,"050-2524542",424,this.getBranchNumber());
-                m1 = new Seller("Joan",323,"052-27904542",423,this.getBranchNumber());
+                m1 = new Seller("Joan", 323, "052-27904542", 423, this.getBranchNumber());
 
                 break;
         }
-        try
-        {
+        try {
 
             //change it later to something more secure
 
@@ -42,18 +39,18 @@ public class Manager extends Employee  {
             String myDriver = "org.gjt.mm.mysql.Driver";
 
             Connection myConn = DriverManager.getConnection
-                    ("jdbc:mysql://localhost:3306/test_db","root","12345");
+                    ("jdbc:mysql://localhost:3306/test_db", "root", "12345");
             Class.forName(myDriver);
 
             String sql = "INSERT INTO employee (name,id,phone,accountNum,branchNum,profession) values (?,?,?,?,?,?)";
             PreparedStatement statement = myConn.prepareStatement(sql);
 
-            statement.setString(1,m1.getName());
-            statement.setInt(2,m1.getId());
-            statement.setString(3,m1.getPhone());
-            statement.setInt(4,m1.getAccountNum());
-            statement.setInt(5,m1.getBranchNumber());
-            statement.setString(6,m1.getJobPos().name());
+            statement.setString(1, m1.getName());
+            statement.setInt(2, m1.getId());
+            statement.setString(3, m1.getPhone());
+            statement.setInt(4, m1.getAccountNum());
+            statement.setInt(5, m1.getBranchNumber());
+            statement.setString(6, m1.getJobPos().name());
 
             statement.executeUpdate();
 
@@ -78,7 +75,7 @@ public class Manager extends Employee  {
             Class.forName(myDriver);
             String sql = "DELETE from employee where empNum = ?";
             PreparedStatement statement = myConn.prepareStatement(sql);
-            statement.setInt( 1, empID);
+            statement.setInt(1, empID);
             statement.execute();
             myConn.close();
         } catch (ClassNotFoundException e) {
@@ -116,7 +113,7 @@ public class Manager extends Employee  {
     @Override
     public String toString() {
         String str = super.toString();
-        str = str + ", Employee's position: " +this.getJobPos()+ ", Employee's branch number: " +this.getBranchNumber();
+        str = str + ", Employee's position: " + this.getJobPos() + ", Employee's branch number: " + this.getBranchNumber();
         return str;
     }
 
@@ -144,17 +141,16 @@ public class Manager extends Employee  {
             }
 
             myConn.close();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-            catch(ClassNotFoundException e){
-                e.printStackTrace();
-            } catch(SQLException e){
-                e.printStackTrace();
-            }
-            return e1;
-        }
+        return e1;
+    }
 
-        public static void main(String [] args){
-        Manager m = new Manager("Guy",1243,"050-112211",234532,2);
+    public static void main(String[] args) {
+        Manager m = new Manager("Guy", 1243, "050-112211", 234532, 2);
         //Employee e2 = new Seller("Joan",323,"052-44444444",423,m.getBranchNumber());
         //m.addEmploee();
         Employee e2 = m.selectDetails(9);
@@ -163,5 +159,6 @@ public class Manager extends Employee  {
         m.updateEpmloyee(e2);
         //m.deleteEmployee();
         //me trying to see if I can push from intellij
+        System.out.print("DONE");
     }
 }
