64d41f9ab6b8ecfc9e70f47733c2a58efef779fa
Roy Bar <roy.bar134@gmail.com>
2018-08-22 13:00:52 +0300

    Improved Login input checks
    -limit input values
    -improved security for password input

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -4,7 +4,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.net.URL;
+import java.util.Arrays;
 
 public class Login extends JFrame {
     private JPanel LoginPanel;
@@ -35,10 +38,55 @@ public class Login extends JFrame {
         pack();
         setVisible(true);
 
+        employeeIDFormattedTextField.addKeyListener(new KeyAdapter() {
+            public void keyTyped(KeyEvent e) {
+                if (employeeIDFormattedTextField.getText().length() >= 9 ) // limits text field to 9 characters
+                    e.consume();
+            }
+        });
+
+        passwordField.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+                if(passwordField.getPassword().length >= 20) // limits password field to 20 characters
+                    e.consume();
+            }
+        });
+
+
         loginButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                new SwingWorker(){ //Open a login input check in a new thread.
+
+                    @Override
+                    protected Object doInBackground() throws Exception {
+                        //get login information and search for a match in DB
+                        String employeeInputId = employeeIDFormattedTextField.getText();
+                        char[] passwordInput = passwordField.getPassword();
+
+                        //Example check
+                        char[] exmp = {'1','2','3','4','5','6','7','8'};
+
+                        //change to DB check
+                        if(employeeInputId.equals("304989171") && Arrays.equals(exmp,passwordInput)){
+                            JOptionPane.showMessageDialog(new JFrame(),"Successful login!");
+                            //Go to main employees page.
+                        }
+
+                        else {
+                            JOptionPane.showMessageDialog(new JFrame(),"Unsuccessful login, please try again.","Invalid input",JOptionPane.ERROR_MESSAGE);
+                        }
+
+                        //Clear password field for security reasons
+                        Arrays.fill(passwordInput,'0');
+                        passwordField.selectAll();
+                        passwordField.setText(null);
+
+                        return null;
+                    }
+                }.execute();
             }
         });
     }
